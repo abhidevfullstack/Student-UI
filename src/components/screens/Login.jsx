f9ac9ec099dbbfad222f9a0ce8a537449ac25fd8
abhidevfullstack <abhishekpurohit8989@gmail.com>
2024-08-22 19:03:38 +0530

    Update Login.jsx

--- src/components/screens/Login.jsx
+++ src/components/screens/Login.jsx
@@ -1,70 +1,82 @@
 import { Alert, Box, Button, Grid, TextField } from "@mui/material";
-import { useEffect, useState } from "react";
-import { notify } from "../../utils/ToastNotification";
-import { TOAST_TYPE } from "../../constants";
-import { getToken, login, storeToken } from "../../services/AdminService";
-import { StatusCodes } from "http-status-codes";
-import { useNavigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-
+import '../../assets/styles/TextInputStyle.css';
+import { useLogin } from "../../custom-hooks/useLogin";
+import { MESSAGES } from "../../constants";
 export function Login() {
 
-    const navigate = useNavigate();
-
-    useEffect(()=>{
-        if(getToken()){
-            navigate("/home");
-        }
-    },[]);
-
-    const [credentials, setCredentials] = useState({ username: '', password: '' });
-
-    const handleInputChange = (event) => {
-        setCredentials({ ...credentials, [event.target.name]: event.target.value });
-    }
-
-    const handleLogin = async () => {
-        try {
-            const response = await login(credentials);
-            if (response.status === StatusCodes.OK) {
-                storeToken(response.data.token);
-                navigate("/home");
-            }
-        } catch (error) {
-            if (error.response.status === StatusCodes.BAD_REQUEST) {
-                notify(error.response.data.message, TOAST_TYPE.ERROR)
-            }
-            else {
-                notify("Something went wrong", TOAST_TYPE.ERROR);
-            }
-
-
-        }
-    }
-
+    const {
+        handleSubmit,
+        handleChange,
+        handleBlur,
+        values,
+        isValid,
+        touched,
+        dirty,
+        errors
+    } = useLogin();
+    
     return (
         <Box>
             <Grid container>
                 <Grid item xs={5}>
-                    <Alert severity="success">LOGIN</Alert>
+                    <Alert severity="success">{MESSAGES.LOGIN_HEADING}</Alert>
                 </Grid>
 
             </Grid>
-            <Grid container sx={{ marginTop: 5 }}>
-                <Grid item xs={5}>
-                    <TextField fullWidth label="Username" name="username" variant="outlined" placeholder="Enter Username" onChange={handleInputChange} />
-                </Grid>
+            
+            <form onSubmit={handleSubmit}>
+                <Grid container sx={{ marginTop: 5 }}>
+                    <Grid item xs={5}>
+                        <TextField 
+                            className="custom-field"
+                            fullWidth 
+                            label="Username" 
+                            id="username"
+                            name="username" 
+                            variant="outlined" 
+                            placeholder={MESSAGES.USERNAME_FIELD_PLACEHOLDER}
+                            value={values.username} 
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            error={touched.username && Boolean(errors.username)}
+                            helperText={touched.username && errors.username}
+                            margin="normal"
+                        />
+                    </Grid>
 
-            </Grid>
-            <Grid container sx={{ marginTop: 3 }}>
-                <Grid item xs={5}>
-                    <TextField fullWidth type="password" label="Password" name="password" variant="outlined" placeholder="Enter Password" onChange={handleInputChange} />
                 </Grid>
-            </Grid>
-            <Button variant="contained" color="primary" sx={{ marginTop: 3 }} onClick={handleLogin}>
-                Login
-            </Button>
-            <ToastContainer/>
+                <Grid container sx={{ marginTop: 3 }}>
+                    <Grid item xs={5}>
+                        <TextField 
+                            className="custom-field"
+                            fullWidth 
+                            type="password" 
+                            label="Password" 
+                            name="password" 
+                            variant="outlined" 
+                            placeholder={MESSAGES.PASSWORD_FIELD_PLACEHOLDER}
+                            value={values.password} 
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            error={touched.password && Boolean(errors.password)}
+                            helperText={touched.password && errors.password}
+                            margin="normal"
+                        />
+                    </Grid>
+                </Grid>
+                <Button 
+                    variant="contained" 
+                    color="primary" 
+                    sx={{ marginTop: 3 }}
+                    type="submit"
+                    disabled={!(isValid && dirty)}
+                >
+                    {MESSAGES.LOGIN_BUTTON_TEXT}
+                </Button>
+            </form>
+
+            <ToastContainer />
         </Box>
     )
 }
