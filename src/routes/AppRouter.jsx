66c685b6329bc42699d7181044aaeafe423cba7e
abhidevfullstack <abhishekpurohit8989@gmail.com>
2024-08-22 19:01:09 +0530

    Update AppRouter.jsx

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -5,20 +5,23 @@ import { StudentsList } from "../components/screens/StudentsList";
 import { EditForm } from "../components/screens/EditForm";
 import { Login } from "../components/screens/Login";
 import { PrivateRoute } from "../components/library/PrivateRoute";
+import { Layout } from "../components/library/Layout";
 
 export function AppRouter() {
     return (
         <div style={{ flexGrow: 1, padding: 15, marginTop: 75 }}>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route element={<PrivateRoute/>}>
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/register-student" element={<RegistrationForm />} />
-                    <Route path="/students-list" element={<StudentsList />} />
-                    <Route path="/edit-form/:id" element={<EditForm/>} />
-                </Route>
-                
-            </Routes>
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route element={<PrivateRoute />}>
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/register-student" element={<RegistrationForm />} />
+                        <Route path="/students-list" element={<StudentsList />} />
+                        <Route path="/edit-form/:id" element={<EditForm />} />
+                    </Route>
+
+                </Routes>
+            </Layout>
         </div>
 
     )
