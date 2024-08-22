9bb1cd9d08bd3ac81307a6771d710513c6866012
abhidevfullstack <abhishekpurohit8989@gmail.com>
2024-08-22 19:03:00 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,3 +4,12 @@ export const TOAST_TYPE = Object.freeze({
     WARNING: 'warning',
     INFO: 'info'
 });
+
+export const MESSAGES = Object.freeze({
+    USERNAME_FIELD_PLACEHOLDER:'Enter Username',
+    PASSWORD_FIELD_PLACEHOLDER:'Enter Password',
+    LOGIN_BUTTON_TEXT:'Login',
+    LOGIN_HEADING:'LOGIN',
+    HOME_WELCOME_TEXT:'Welcome to Student CRUD Application.',
+    HOME_DESCRIPTION:'In this app, you can register a student, search students, delete the student and modify student data as well'
+});
